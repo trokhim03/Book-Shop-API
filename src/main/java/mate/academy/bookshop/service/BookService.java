348486481e6b0e3b5487bda3c515da90d44ed2c2
trokhim03 <trokhimchuk.003@gmail.com>
2025-01-28 11:59:50 +0200

    pr-6

--- src/main/java/mate/academy/bookshop/service/BookService.java
+++ src/main/java/mate/academy/bookshop/service/BookService.java
@@ -4,11 +4,12 @@ import java.util.List;
 import mate.academy.bookshop.dto.BookDto;
 import mate.academy.bookshop.dto.BookSearchParameters;
 import mate.academy.bookshop.dto.CreateBookRequestDto;
+import org.springframework.data.domain.Pageable;
 
 public interface BookService {
     BookDto save(CreateBookRequestDto createBookRequestDto);
 
-    List<BookDto> findAll();
+    List<BookDto> findAll(Pageable pageable);
 
     BookDto findBookById(Long id);
 
--- src/main/java/mate/academy/bookshop/service/BookServiceImpl.java
+++ src/main/java/mate/academy/bookshop/service/BookServiceImpl.java
@@ -10,6 +10,7 @@ import mate.academy.bookshop.mapper.BookMapper;
 import mate.academy.bookshop.model.Book;
 import mate.academy.bookshop.repository.BookRepository;
 import mate.academy.bookshop.repository.specification.book.BookSpecificationBuilder;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
@@ -27,8 +28,8 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
-    public List<BookDto> findAll() {
-        return bookRepository.findAll().stream()
+    public List<BookDto> findAll(Pageable pageable) {
+        return bookRepository.findAll(pageable).stream()
                 .map(bookMapper::toDto)
                 .toList();
     }
