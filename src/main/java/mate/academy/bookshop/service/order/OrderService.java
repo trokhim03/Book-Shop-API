277fd957b73086fc9ef71abf04e3bc6aa4eb164b
trokhim03 <trokhimchuk.003@gmail.com>
2025-03-10 10:20:39 +0200

    added docker

--- src/main/java/mate/academy/bookshop/service/order/OrderService.java
+++ src/main/java/mate/academy/bookshop/service/order/OrderService.java
@@ -5,10 +5,11 @@ import mate.academy.bookshop.dto.order.OrderRequestDto;
 import mate.academy.bookshop.dto.order.OrderResponseDto;
 import mate.academy.bookshop.dto.order.OrderUpdateStatusDto;
 import mate.academy.bookshop.dto.orderitem.OrderItemResponseDto;
+import mate.academy.bookshop.model.User;
 import org.springframework.data.domain.Pageable;
 
 public interface OrderService {
-    OrderResponseDto createOrderByUserId(Long userId, OrderRequestDto orderRequestDto);
+    OrderResponseDto createOrderByUser(User user, OrderRequestDto orderRequestDto);
 
     List<OrderResponseDto> getOrderByUserId(Pageable pageable, Long userId);
 
